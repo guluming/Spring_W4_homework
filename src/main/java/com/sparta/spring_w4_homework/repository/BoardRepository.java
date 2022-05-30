01cb5780113075af58c88153f6b4e8ef7fe5874e
guluming <guluming@naver.com>
2022-05-30 10:10:00 +0900

    repository 클래스

---
+++ src/main/java/com/sparta/spring_w4_homework/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.spring_w4_homework.repository;
+
+import com.sparta.spring_w4_homework.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
