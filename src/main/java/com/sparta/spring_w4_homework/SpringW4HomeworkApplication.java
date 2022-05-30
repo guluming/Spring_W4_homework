d959cbf3cac640e00874196f9c247fe50ee8a29d
guluming <guluming@naver.com>
2022-05-30 10:04:23 +0900

    설정 및 어플리케이션 & 테스트 파일

---
+++ src/main/java/com/sparta/spring_w4_homework/SpringW4HomeworkApplication.java
@@ -0,0 +1,15 @@
+package com.sparta.spring_w4_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringW4HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringW4HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/spring_w4_homework/SpringW4HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_w4_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringW4HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
