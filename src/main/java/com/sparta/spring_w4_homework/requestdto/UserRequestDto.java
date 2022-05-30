33e55580433096f5fe831f9b8308044fd1bb0c2d
guluming <guluming@naver.com>
2022-05-30 15:37:28 +0900

    생성일 수정일 변수 삭제 / toentity 사용 중지

--- src/main/java/com/sparta/spring_w4_homework/requestdto/UserRequestDto.java
+++ src/main/java/com/sparta/spring_w4_homework/requestdto/UserRequestDto.java
@@ -3,10 +3,6 @@ package com.sparta.spring_w4_homework.requestdto;
 import com.sparta.spring_w4_homework.model.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
@@ -14,17 +10,11 @@ public class UserRequestDto {
     private String username;
     private String password;
     private String passwordchk;
-//    @CreatedDate
-//    private LocalDateTime createdAt;
-//    @LastModifiedDate
-//    private LocalDateTime modifiedAt;
 
-    public User toEntity(){
-        return User.builder()
-                .username(username)
-                .password(password)
-//                .createdAt(createdAt)
-//                .modifiedAt(modifiedAt)
-                .build();
-    }
+//    public User toEntity(){
+//        return User.builder()
+//                .username(username)
+//                .password(password)
+//                .build();
+//    }
 }
