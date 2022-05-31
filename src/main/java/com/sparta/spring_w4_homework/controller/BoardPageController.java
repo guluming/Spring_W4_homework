d0c4869f3b77eb9dc38b1b9bb452750262eec049
guluming <guluming@naver.com>
2022-05-31 16:28:12 +0900

    사용중지

--- src/main/java/com/sparta/spring_w4_homework/controller/BoardPageController.java
+++ src/main/java/com/sparta/spring_w4_homework/controller/BoardPageController.java
@@ -1,37 +1,66 @@
 package com.sparta.spring_w4_homework.controller;
 
+import com.sparta.spring_w4_homework.requestdto.UserRequestDto;
+import com.sparta.spring_w4_homework.security.UserDetailsImpl;
+import com.sparta.spring_w4_homework.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
 public class BoardPageController {
 
+//    private final UserService userService;
+
+//    @Autowired
+//    public BoardPageController(UserService userService) {
+//        this.userService = userService;
+//    }
+
+    //메인 화면
+//    @GetMapping("/")
+//    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails){
+//        model.addAttribute("username", userDetails.getUsername());
+//        return "index";
+//    }
+
     //로그인 페이지
-    @GetMapping("/user/login")
-    public String login() {
-        return "login";
-    }
+//    @GetMapping("/user/loginView")
+//    public String login() {
+//        return "login";
+//    }
 
     //회원가입 페이지
-    @GetMapping("/user/signup")
-    public String signup() {
-        return "signup";
-    }
+//    @GetMapping("/user/signup")
+//    public String signup() {
+//        return "signup";
+//    }
 
-    //상세 페이지
-    @GetMapping("/board/view/{id}")
-    public String boardview(@PathVariable Long id, Model model) {
-        model.addAttribute("id", id);
-        return "view";
-    }
+    //회원가입
+//    @PostMapping("/user/signup")
+//    public String signup(UserRequestDto params){
+//        userService.signup(params);
+//        return "redirect:/user/login";
+////        return userService.signup(params);
+//    }
 
-    //수정 페이지
-    @GetMapping("/board/edit/{id}")
-    public String boardedit(@PathVariable Long id, Model model) {
-        model.addAttribute("id", id);
-        return "edit";
-    }
+//    //상세 페이지
+//    @GetMapping("/board/view/{id}")
+//    public String boardview(@PathVariable Long id, Model model) {
+//        model.addAttribute("id", id);
+//        return "view";
+//    }
+//
+//    //수정 페이지
+//    @GetMapping("/board/edit/{id}")
+//    public String boardedit(@PathVariable Long id, Model model) {
+//        model.addAttribute("id", id);
+//        return "edit";
+//    }
 }
