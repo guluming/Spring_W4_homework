b5b2a7328da0d2610af5577b6a1814d8375d5391
guluming <guluming@naver.com>
2022-05-31 16:21:34 +0900

    토큰 요청용 Dto

---
+++ src/main/java/com/sparta/spring_w4_homework/requestdto/JwtRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.spring_w4_homework.requestdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JwtRequestDto {
+
+    private String username;
+    private String password;
+}
