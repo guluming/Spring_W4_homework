2ccd34c0f808ea0f80754f5fd406f876de3f9250
guluming <guluming@naver.com>
2022-05-31 16:16:47 +0900

    builder 사용 제거 / 로그인&회원가입 api 수정

--- src/main/java/com/sparta/spring_w4_homework/controller/UserApiController.java
+++ src/main/java/com/sparta/spring_w4_homework/controller/UserApiController.java
@@ -1,12 +1,17 @@
 package com.sparta.spring_w4_homework.controller;
 
+import com.sparta.spring_w4_homework.requestdto.JwtRequestDto;
 import com.sparta.spring_w4_homework.requestdto.UserRequestDto;
+import com.sparta.spring_w4_homework.responsedto.JwtResponseDto;
 import com.sparta.spring_w4_homework.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+//@AllArgsConstructor
 @RestController
+@RequestMapping("/user")
 public class UserApiController {
 
     private final UserService userService;
@@ -18,16 +23,33 @@ public class UserApiController {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    //중복 로그인 검사
+//    @GetMapping("/api/usercheck")
+//    public void usercheck(){
+//
+//    }
+
     //회원가입
-    @PostMapping("/user/signup")
-    public String signup(@RequestBody UserRequestDto params){
-        //userService.signup(params);
+//    @PostMapping("/user/signup")
+//    public String signup(UserRequestDto params){
+////        userService.signup(params);
+////        return "redirect:/user/login";
+//        return userService.signup(params);
+//    }
+
+    //회원가입
+    @PostMapping(value = "/signup", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String signup(@RequestBody UserRequestDto params) {
         return userService.signup(params);
     }
 
-//    //회원로그인
-//    @PostMapping("/user/login")
-//    public String iogin(Long id, @RequestBody MemberRequestDto params){
-//        return memberService.memberlogin(id, params);
-//    }
+    //회원로그인
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public JwtResponseDto login(@RequestBody JwtRequestDto params) {
+        try {
+            return userService.login(params);
+        } catch (Exception e) {
+            return new JwtResponseDto("닉네임 또는 패스워드를 확인해주세요");
+        }
+    }
 }
--- src/main/java/com/sparta/spring_w4_homework/requestdto/UserRequestDto.java
+++ src/main/java/com/sparta/spring_w4_homework/requestdto/UserRequestDto.java
@@ -1,20 +1,13 @@
 package com.sparta.spring_w4_homework.requestdto;
 
-import com.sparta.spring_w4_homework.model.User;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@NoArgsConstructor
+//@NoArgsConstructor
 @Getter
+@Setter
 public class UserRequestDto {
     private String username;
     private String password;
     private String passwordchk;
-
-//    public User toEntity(){
-//        return User.builder()
-//                .username(username)
-//                .password(password)
-//                .build();
-//    }
 }
